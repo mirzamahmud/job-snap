90924095a557334282a64c1bc14007b4b12fae56
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2024-11-18 01:11:09 +0600

    add_text_style

---
+++ lib/app/utils/app_text_style.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// ----------- heading 1 -----------
+const TextStyle h1 = TextStyle(fontSize: 36);
+// ----------- heading 2 -----------
+const TextStyle h2 = TextStyle(fontSize: 32);
+// ----------- heading 3 -----------
+const TextStyle h3 = TextStyle(fontSize: 28);
+// ----------- heading 4 -----------
+const TextStyle h4 = TextStyle(fontSize: 24);
+// ----------- heading 5 -----------
+const TextStyle h5 = TextStyle(fontSize: 20);
+// ----------- heading 6 -----------
+const TextStyle h6 = TextStyle(fontSize: 18);
+// ----------- body large -----------
+const TextStyle bodyLarge = TextStyle(fontSize: 18);
+// ----------- body medium -----------
+const TextStyle bodyMedium = TextStyle(fontSize: 16);
+// ----------- body small -----------
+const TextStyle bodySmall = TextStyle(fontSize: 14);
+// ----------- body extra small -----------
+const TextStyle bodyExtraSmall = TextStyle(fontSize: 12);
+// ----------- label large -----------
+const TextStyle labelLarge = TextStyle(fontSize: 16);
+// ----------- label medium -----------
+const TextStyle labelMedium = TextStyle(fontSize: 14);
+// ----------- label small -----------
+const TextStyle labelSmall = TextStyle(fontSize: 12);
+// ----------- label extra small -----------
+const TextStyle labelExtraSmall = TextStyle(fontSize: 10);
